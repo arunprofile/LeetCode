3bd701ac404581a129ed0bf54b78618f50e9d37d
Arun <aruntp4u@gmail.com>
2023-08-04 21:39:04 +0530

    solution

--- two-sum.py
+++ two-sum.py
@@ -10,4 +10,14 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 '''
 #Solution:
+#Using bruteforce algo
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        if n < 2:
+            return -1
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
